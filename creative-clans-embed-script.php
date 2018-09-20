fef767ffeeeaacd844af3da9061971a5e5812bd3
dchenk <dcherchenko@gmail.com>
2018-09-20 13:10:03 -0700

    Various cleanups and improvements

--- creative-clans-embed-script.php
+++ creative-clans-embed-script.php
@@ -2,10 +2,9 @@
 /*
 Plugin Name: Creative Clans Embed Script
 Plugin URI: http://www.creativeclans.nl
-Description: Gives the possibility to add scripts to the beginning and/or the end of the 'content' of any page or post. 
-Version: 1.2
-Author: Guido Tonnaer
-Author URI: http://www.creativeclans.nl
+Description: Gives the possibility to add scripts to the beginning and/or the end of the 'content' of any page or post.
+Version: 10.0
+Author: Guido Tonnaer and Wider Webs
 
 Copyright 2010-2012 Guido Tonnaer
 
@@ -24,83 +23,60 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-/**
- * Determine the location.
- */
-$ccembedscriptpluginpath = WP_CONTENT_URL.'/plugins/'.plugin_basename(dirname(__FILE__)).'/';
-
-/**
- * Hook the_content to output the scripts added to the page or post.
- */
+// Output the scripts.
+function ccembedscript_display_hook($content = '') {
+	global $post;
+	return get_post_meta($post->ID, '_ccembedscripttexttop', true) . $content . get_post_meta($post->ID, '_ccembedscripttext', true);
+}
 add_filter('the_content', 'ccembedscript_display_hook');
 add_filter('the_excerpt', 'ccembedscript_display_hook');
-	
-/**
- * output the scripts.
- */
-function ccembedscript_display_hook($content='') {
-	global $post;
-  return get_post_meta($post->ID, '_ccembedscripttexttop', true) . $content . get_post_meta($post->ID, '_ccembedscripttext', true);
-}
 
-/**
- * Displays a box that allows users to insert the scripts for the post or page
- */
+// Displays a box that allows users to insert the scripts for the post or page.
 function ccembedscript_meta($post) {
-  // Use nonce for verification
-  wp_nonce_field( plugin_basename( __FILE__ ), 'ccembedscript_noncename' );
-
+	// Use nonce for verification.
+	wp_nonce_field(plugin_basename( __FILE__ ), 'ccembedscript_noncename');
 	?>
-	<label for="ccembedscripttexttop"><?php _e('Scripts to be inserted at the top','ccembedscript') ?></label><br />
-  <textarea id="ccembedscripttexttop" name="ccembedscripttexttop" /><?php echo get_post_meta($post->ID,'_ccembedscripttexttop',true); ?></textarea><br />
-	<label for="ccembedscripttext"><?php _e('Scripts to be inserted at the bottom','ccembedscript') ?></label><br />
-  <textarea id="ccembedscripttext" name="ccembedscripttext" /><?php echo get_post_meta($post->ID,'_ccembedscripttext',true); ?></textarea>
-	<?php
+	<style> #ccembedscripttexttop, #ccembedscripttext { width: 100%; height: 80px; } </style>
+	<label for="ccembedscripttexttop">Scripts inserted at the top</label><br>
+	<textarea id="ccembedscripttexttop" name="ccembedscripttexttop"><?php echo get_post_meta($post->ID, '_ccembedscripttexttop', true); ?></textarea><br>
+	<label for="ccembedscripttext">Scripts inserted at the bottom<label><br>
+	<textarea id="ccembedscripttext" name="ccembedscripttext"><?php echo get_post_meta($post->ID, '_ccembedscripttext', true); ?></textarea><?php
 }
 
-/**
- * Add the box defined above to post and page edit screens.
- */
+// Add the box defined above to post and page edit screens.
 function ccembedscript_meta_box() {
-	add_meta_box('ccembedscript','Creative Clans Embed Script','ccembedscript_meta','post','side');
-	add_meta_box('ccembedscript','Creative Clans Embed Script','ccembedscript_meta','page','side');
+	add_meta_box('ccembedscript', 'Creative Clans Embed Script', 'ccembedscript_meta', 'post', 'side');
+	add_meta_box('ccembedscript', 'Creative Clans Embed Script', 'ccembedscript_meta', 'page', 'side');
 }
 add_action('admin_menu', 'ccembedscript_meta_box');
 
-/**
- * If the post is inserted, save the script.
- */
+// If the post is inserted, save the script.
 function ccembedscript_insert_post($pID) {
+	// If the function is called by the WP autosave feature, nothing must be saved.
+	if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+		return;
+	}
 
-  // if the function is called by the WP autosave feature, nothing must be saved
-  if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE )
-    return;
-    
-  // verify this came from the our screen and with proper authorization,
-  // because save_post can be triggered at other times
+	// Verify this came from our screen and with proper authorization,
+	// because save_post can be triggered at other times.
 
-  if ( !wp_verify_nonce( $_POST['ccembedscript_noncename'], plugin_basename( __FILE__ ) ) )
-      return;
+	if (!wp_verify_nonce($_POST['ccembedscript_noncename'], plugin_basename( __FILE__ ))) {
+		return;
+	}
 
-  
-  // Check permissions
-  if ( 'page' == $_POST['post_type'] ) 
-  {
-    if ( !current_user_can( 'edit_page', $pID ) )
-        return;
-  }
-  else
-  {
-    if ( !current_user_can( 'edit_post', $pID ) )
-        return;
-  }
+	// Check permissions.
+	if ('page' == $_POST['post_type']) {
+		if (!current_user_can('edit_page', $pID)) {
+			return;
+		}
+	} else if (!current_user_can('edit_post', $pID)) {
+		return;
+	}
 
-  // OK, we're authenticated: we need to find and save the data
-  $text = (isset($_POST['ccembedscripttexttop'])) ? $_POST['ccembedscripttexttop'] : '';
-  update_post_meta($pID, '_ccembedscripttexttop', $text);
-  $text = (isset($_POST['ccembedscripttext'])) ? $_POST['ccembedscripttext'] : '';
-  update_post_meta($pID, '_ccembedscripttext', $text);
+	// We're authenticated. Find and save the data.
+	$text = (isset($_POST['ccembedscripttexttop'])) ? $_POST['ccembedscripttexttop'] : '';
+	update_post_meta($pID, '_ccembedscripttexttop', $text);
+	$text = (isset($_POST['ccembedscripttext'])) ? $_POST['ccembedscripttext'] : '';
+	update_post_meta($pID, '_ccembedscripttext', $text);
 }
 add_action('save_post', 'ccembedscript_insert_post');
-
-?>
--- readme.txt
+++ readme.txt
@@ -2,9 +2,8 @@
 Contributors: tonnaer
 Donate link: http://www.creativeclans.nl
 Tags: script, embed, plugin, creative clans
-Requires at least: 2.7
-Tested up to: 4.1.2
-Stable tag: 1.2
+Tested up to: 4.9.8
+Stable tag: 10.0
 
 A plugin to add scripts to the beginning and/or the end of the 'content' of any post or page. 
 
@@ -16,19 +15,10 @@ The scripts will be added to the beginning and/or end of the 'content' of the pa
 A very easy way to add for example Google Adwords conversion tracking code to a page.
 And with the new feature, you can also use it to add the Google Website Optimizer code and splittest your landingpages.
 
-== Installation ==
-
-1. There are two ways to install the plugin:
-   a) Use the 'automatic install from the Wordpress plugin directory' option in the
-      Wordpress New widgets admin panel
-   OR
-   b) Download the zip-file, and use the 'upload' option in the Wordpress New widgets
-      admin panel
-2. Activate the plugin through the 'Plugins' menu in WordPress
-
-Now you're ready to go! Go to a post or page, open the edit screen, and on the right should appear a 'Scripts to be inserted ' box. All you have to do is insert the scripts you want to add to the beginning and/or the end of the body of the page or post.
-    
 == Changelog ==
+
+= 10.0 =
+* Various cleanups and improvements.
 
 = 1.2 =
 * Corrected bug that caused loss of the embed script snippets with WP autosave feature
